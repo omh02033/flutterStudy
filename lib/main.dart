09f33a3f54090ab9592562a8b7282c1281aac372
오명훈 <omh02033@gmail.com>
2022-07-06 17:51:33 +0900

    feat: study

--- lib/main.dart
+++ lib/main.dart
@@ -9,75 +9,62 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.menu, color: Colors.black),
-          title: Text('내 앱', style: TextStyle(
-            color: Colors.black,
-          ),),
-          actions: [
-            TextButton(
-              onPressed: (){print('clicked');},
-              child: Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(50),
-                    child: Image.asset('assets/background.png', width: 30),
-                  ),
-                  SizedBox(width: 10),
-                  Text('오명훈')
-                ],
-              )
-            ),
+        appBar: AppBar(),
+        body: ListView(
+          children: [
+            UserField(),
+            UserField(),
+            UserField(),
           ],
-          backgroundColor: Colors.white,
         ),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image.asset('assets/background.png', width: 150),
-              Expanded(
-                child: Container(
-                  padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('나 팝니다', style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 25
-                      )),
-                      Text('금호동 3가', style: TextStyle(
-                        color: Color(0xffcccccc)
-                      )),
-                      Text('2,030,000,000,000 원', style: TextStyle(
-                        fontSize: 15
-                      ),),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Icon(Icons.favorite),
-                          Text('4')
-                        ],
-                      )
-                    ],
-                  ),
-                )
-              )
-            ],
+        bottomNavigationBar: BottomNavigation(),
+      )
+    );
+  }
+}
+
+class UserField extends StatelessWidget {
+  const UserField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      // padding: EdgeInsets.all(10),
+      onPressed: () { print("Hello"); },
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(50),
+            child: Image.asset('assets/background.png', width: 50),
           ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          selectedItemColor: Color(0xffE83C77),
-          unselectedItemColor: Color(0xffcccccc),
-          items: [
-            BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
-            BottomNavigationBarItem(label: 'Search', icon: Icon(Icons.search)),
-            BottomNavigationBarItem(label: 'Profile', icon: Icon(Icons.account_circle_outlined)),
-          ],
-        ),
+          SizedBox(
+            width: 20
+          ),
+          Text('홍길동', style: TextStyle(
+            fontSize: 18,
+            color: Colors.black
+          ),)
+        ],
       ),
+    );
+  }
+}
+
+class BottomNavigation extends StatelessWidget {
+  const BottomNavigation({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: BottomNavigationBar(
+      showUnselectedLabels: false,
+      showSelectedLabels: false,
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.phone), label:'phone'),
+        BottomNavigationBarItem(icon: Icon(Icons.message), label:'message'),
+        BottomNavigationBarItem(icon: Icon(Icons.contact_page), label:'document'),
+      ],
+    ),
     );
   }
 }
