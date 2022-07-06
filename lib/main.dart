68bad7e39025e9d2f952983bdf220e8dd2ce1d11
오명훈 <omh02033@gmail.com>
2022-07-06 14:25:21 +0900

    feat: flexible test

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to OMH'),
-        ),
-        body: Center(
-          child: Text('Hello world'),
+        appBar: AppBar(),
+        body: Container(
+          height: 150,
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Image.asset('assets/background.png', width: 150),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('나 팝니다', style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 25
+                      )),
+                      Text('금호동 3가', style: TextStyle(
+                        color: Color(0xffcccccc)
+                      )),
+                      Text('2,030,000,000,000 원', style: TextStyle(
+                        fontSize: 15
+                      ),),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Icon(Icons.favorite),
+                          Text('4')
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              )
+            ],
+          ),
         ),
       ),
     );
